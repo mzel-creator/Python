9a577911a35232fcdaea7fa3dfb76ddda3899798
mzel <zmae@mail.ru>
2021-05-11 16:20:50 +0300

    Не очень красиво, но вроде бы работает

---
+++ Task_2.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+num = input("Введите число от 1 до 20: ")
+s1 = "процент"
+s2 = "процента"
+s3 = "процентов"
+if num == 1:
+    print (str(num) + s1)
+elif num == 2:
+    print (str(num) + s2)
+elif num == 3:
+    print (str(num) + s2)
+elif num == 4:
+    print (str(num) + s2)
+else:
+    print (str(num) + s3)
