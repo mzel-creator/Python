ab6776fbbf77f0aec9bdaa0eced97ea6043c8c70
mzel <zmae@mail.ru>
2021-05-16 12:34:21 +0300

    Задание 1 - урок 2

---
+++ Task_2_1.py
@@ -0,0 +1,15 @@
+#1
+a = 15 * 3
+b = 15 / 3
+c = 15 // 2
+d = 15 ** 2
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+
+#2
+print(isinstance(a, int))
+print(isinstance(b, float))
+print(isinstance(c, int))
+print(isinstance(d, int))
