c93eb52bec1943e8759a7089959238ae4f30d119
mzel <zmae@mail.ru>
2021-05-18 10:42:33 +0300

    Задание 1 - урок 2

--- Task_2_1.py
+++ Task_2_1.py
@@ -1,4 +1,5 @@
 #1
+
 a = 15 * 3
 b = 15 / 3
 c = 15 // 2
@@ -9,12 +10,14 @@ print(type(c))
 print(type(d))
 
 #2
+
 print(isinstance(a, int))
 print(isinstance(b, float))
 print(isinstance(c, int))
 print(isinstance(d, int))
 
 #3
+
 result = [a, b, c, d]
 for i in result:
     print(type(i ))
