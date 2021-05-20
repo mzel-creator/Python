b86d0a468af4cd737c8089273904984f9bd70097
mzel <zmae@mail.ru>
2021-05-20 20:15:49 +0300

    Задание 1 - урок 2

---
+++ Task_3_3.py
@@ -0,0 +1,9 @@
+def thesaurus(*names):
+    res = {}
+    for name in names:
+        key = name[0].capitalize()
+        if key not in res:
+            res[key] = []
+        res[key].append(name)
+    return res
+print(thesaurus('Иван', 'Мария', 'Петр', 'Илья', 'Михаил', 'Владимир', 'Виталий'))
