f3d1639da20ed65fe39be3eec9a6bc0b7f724f17
mzel <zmae@mail.ru>
2021-06-01 16:17:17 +0300

    Задание 1 - урок 6

--- Task_6_1.py
+++ Task_6_1.py
@@ -1,4 +1,5 @@
 with open("nginx_logs.txt", "r", encoding="utf-8") as file:
     content = ((line.split()[0], line.split()[5][1:], line.split()[6]) for line in file)
     for i in content:
+
         print(i)
