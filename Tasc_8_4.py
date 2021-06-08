5e7bdfb1dfef9afa537d2751fe7565e68a4421da
mzel <zmae@mail.ru>
2021-06-08 17:11:28 +0300

    Задание 3 - Урок 8

---
+++ Tasc_8_4.py
@@ -0,0 +1,21 @@
+def val_checker(l_func):
+    def _val_checker(func):
+        def wrapper(num):
+            if l_func(num):
+                print(func(num))
+            else:
+                raise ValueError(f"wrong val {num}")
+
+        return wrapper
+
+    return _val_checker
+
+
+@val_checker(lambda x: x > 0)
+def calc_cube(x):
+    return x ** 3
+
+try:
+    a = calc_cube(55)
+except ValueError as err:
+    print(err)
